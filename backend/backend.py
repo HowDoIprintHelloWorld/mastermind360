400e19a65fbfdc321b828efad5371df5744a11f4
HowDoIprintHelloWorld <87938212+HowDoIprintHelloWorld@users.noreply.github.com>
2023-01-22 17:54:29 +0100

    added basic foundation for web API

--- backend/backend.py
+++ backend/backend.py
@@ -1,4 +1,7 @@
 from game import Board, genId
+from flask import Flask, jsonify, request
+from tools import gameById, playerById
+
 
 """
 Steps:
@@ -7,14 +10,48 @@ Steps:
     -> Detect board from ID
 """
 
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def main():
+    return "Enter a game ID!"
+
+
+@app.route("/game/<id>/<player>") #, methods=["GET", "POST"])
+def game(id, player):
+    game = gameById(games, id)
+    board = None
+    if not game:
+        return jsonify({"message":"unknown game with such ID"})
+    print("Player: "+player)
+    board = playerById(game, player)
+    if board == None:
+        return jsonify({"message":"unknown player with such ID"})
+        
+    args = request.args
+    if "action" in args.keys():
+        action = args["action"]
+        # if action.strip() == ""
+        return action
+    else:
+        return jsonify({"message":"no action specified"})
+
 
 
 
 def simulate():
     r = [2, 2]
-    id = genId()
+    id = genId("board")
     test = Board(False, 4, 6, id)
-    test.addPlayer(id)
+    playerId = genId()
+    test.addPlayer(playerId)
+    print(f"Game made! {id}/{playerId}")
+    return test
+    
+    
+def siminput(test):    
     while r[0] != 0:
         r = test.makeGuess(id, input(">> ").split())
         print(*r)
@@ -22,4 +59,8 @@ def simulate():
 
 
 if __name__ == "__main__":
-    simulate()
+    games = []
+    test = simulate()
+    games.append(test)
+    #siminput(test)
+    app.run()
